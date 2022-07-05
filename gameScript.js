aa370565accb81c7155bea2f41af6a3f7067f141
AlbanMarku <albanmarku@outlook.com>
2022-07-05 20:37:50 +0100

    Added game loop function.

--- gameScript.js
+++ gameScript.js
@@ -6,21 +6,45 @@ function computerPlay () { // Get random item from array list.
     return itemSelect[randomChoice]
 }
 
-function playRound(playerInput, computerInput) {
+function playRound(playerInput, computerInput) { // Determines winner by comparing pInput to cInput.
 
     if ((playerInput === "Rock" && computerInput === "Scissors") || (playerInput === "Paper" && computerInput === "Rock") || (playerInput === "Scissors" && computerInput === "Paper")) {
         console.log("Player wins!");
-        console.log(playerInput, computerInput);
         return "win"
+
     } else if (playerInput === computerInput) {
         console.log("It's a draw. Stand down.")
-        console.log(playerInput, computerInput);
         return "draw"
+
     } else {
         console.log("Player loses.")
-        console.log(playerInput, computerInput);
         return "lose"
     }
 }
 
-playRound("Paper", computerPlay());
+function game() {
+
+    let pScore = 0;
+    let cScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        let results = playRound("Rock", computerPlay());
+        
+        switch (results) {
+            case "win":
+                pScore++;
+                console.log("Player won that round.")
+                break;
+            case "lose":
+                cScore++;
+                console.log("Computer won that round.")
+                break;
+            case "draw":
+                console.log("Nobody won that round.")
+                break;
+            default:
+                console.log("I'm going to honest here, I don't know why I'm here.")
+        }
+    }
+    
+}
