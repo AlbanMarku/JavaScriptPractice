ff72dee1a50edeeab37e3a37d00614c0cd52305e
AlbanMarku <albanmarku@outlook.com>
2022-07-05 20:21:48 +0100

    Added round play function and fixed computerPlay return.

--- gameScript.js
+++ gameScript.js
@@ -3,5 +3,24 @@ function computerPlay () { // Get random item from array list.
     const itemSelect = ["Rock", "Paper", "Scissors"];
     const randomChoice = Math.floor(Math.random() * itemSelect.length);
 
-    return randomChoice
+    return itemSelect[randomChoice]
 }
+
+function playRound(playerInput, computerInput) {
+
+    if ((playerInput === "Rock" && computerInput === "Scissors") || (playerInput === "Paper" && computerInput === "Rock") || (playerInput === "Scissors" && computerInput === "Paper")) {
+        console.log("Player wins!");
+        console.log(playerInput, computerInput);
+        return "win"
+    } else if (playerInput === computerInput) {
+        console.log("It's a draw. Stand down.")
+        console.log(playerInput, computerInput);
+        return "draw"
+    } else {
+        console.log("Player loses.")
+        console.log(playerInput, computerInput);
+        return "lose"
+    }
+}
+
+playRound("Paper", computerPlay());
